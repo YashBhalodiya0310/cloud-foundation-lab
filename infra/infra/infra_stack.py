5c09809c13ac1b44b4bf7921eb8d3d171edbd207
YashBhalodiya0310 <yashbhalodiya0310@gmail.com>
2026-01-18 21:13:18 +0000

    Add CDK infra project with S3 + DynamoDB stack

---
+++ infra/infra/infra_stack.py
@@ -0,0 +1,36 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3,
+    aws_dynamodb as dynamodb,
+    Tags,
+    RemovalPolicy,
+)
+from constructs import Construct
+
+
+class InfraStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(
+            self,
+            "ArtifactsBucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        table = dynamodb.Table(
+            self,
+            "AppTable",
+            partition_key=dynamodb.Attribute(
+                name="pk",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        Tags.of(bucket).add("project", "cloud-foundation-lab")
+        Tags.of(table).add("project", "cloud-foundation-lab")
